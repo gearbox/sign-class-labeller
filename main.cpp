2197ebdccb478209439520c1edf67ef645892292
Vlad Shakhuro <shahurik@ya.ru>
2014-05-29 21:20:07 +0400

    Fix exception handling in application

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
 #include "controller.h"
 #include "view.h"
 
+class MyApplication : public QApplication
+{
+public:
+  MyApplication(int &argc, char **argv): QApplication(argc, argv) {}
+
+private:
+  bool notify(QObject *receiver, QEvent *event)
+  {
+    try {
+      return QApplication::notify( receiver, event );
+    } catch ( ... ) {
+      return false;
+    }
+  }
+};
+
 int main(int argc, char *argv[]) {
   bool block_opt = !(argc == 2 && QString(argv[1]) == "--no-block-next");
 
-  QApplication app(argc, argv);
+  MyApplication app(argc, argv);
 
   View view;
   Controller controller(&view, block_opt);
